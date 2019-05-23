ab21a3fb3c0f01ddba1802b3804046400529df28
jsochacki <john.sochacki@viasat.com>
2019-05-23 11:48:26 -0400

    Initial file adding to start project

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # cuFFT_Benchmarking
 Here I benchmark different cuFFT sizes and Plans along with some other operations
+
+Current VS Code settings i'm working through
+   "remote.SSH.showLoginTerminal": true,
+   "remote.extensionKind": {
+      "peterjausovec.vscode-docker": "workspace",
+      "ms-vscode-remote.remote-containers": "workspace",
+      "ms-kubernetes-tools.vscode-kubernetes-tools": "workspace",
+      "ms-vsliveshare.vsliveshare-pack": "workspace",
+      "ms-vsliveshare.vsliveshare": "workspace",
+      "ms-vsliveshare.vsliveshare-audio": "workspace",
+      "alefragnani.bookmarks": "workspace"
+   },
+   "remote.SSH.defaultExtensions": [
+      "peterjausovec.vscode-docker",
+      "ms-vscode-remote.remote-containers",
+      "ms-kubernetes-tools.vscode-kubernetes-tools",
+      "ms-vsliveshare.vsliveshare-pack",
+      "ms-vsliveshare.vsliveshare",
+      "ms-vsliveshare.vsliveshare-audio",
+      "alefragnani.bookmarks"
+   ],
+   "remote.containers.defaultExtensions": [
+      "peterjausovec.vscode-docker",
+      "ms-vscode-remote.remote-containers",
+      "ms-kubernetes-tools.vscode-kubernetes-tools",
+      "ms-vsliveshare.vsliveshare-pack",
+      "ms-vsliveshare.vsliveshare",
+      "ms-vsliveshare.vsliveshare-audio",
+      "alefragnani.bookmarks"
+   ]
---
+++ src/header.h
@@ -0,0 +1,99 @@
+#ifndef GPUFRACTIONALRESAMPLIGNFILTER_H_
+#define GPUFRACTIONALRESAMPLIGNFILTER_H_
+
+//required for compile if file is a .cpp file
+//#include <cuda_runtime.h>
+
+//required for types used
+#include <stdint.h>
+
+typedef struct
+{
+   float i;
+   float q;
+} complex64;
+
+//TODO just for development, can delete
+const float MY_PI = 3.14159265358979323846;
+
+#define CU_FUNC_CALLED_FROM_CPP extern "C"
+
+#define R_MAC 1024
+#define K_MAC 31
+#define POLYNOMIAL_ORDER_MAC 5
+//POLYNOMIAL_ORDER + 1
+#define NCOLUMNS_MAC 6
+#define TPS_MAC 32U
+#define SPB_MAC 24U
+
+#define NCO_Size_MAC 1UL << 41
+#define NCO_Bit_Truncation_MAC 1UL << 31
+
+//Filter design parameters
+extern const uint16_t R;
+extern const uint16_t K;
+extern const uint16_t POLYNOMIAL_ORDER;
+
+extern uint32_t NUMBER_OF_INPUT_SAMPLES;
+extern uint32_t NUMBER_OF_OUTPUT_SAMPLES;
+
+// The NCO and all associated NCO data needs to be uint64
+extern uint64_t NCO_Size;
+extern uint64_t NCO_Bit_Truncation;
+extern uint64_t NCO;
+
+extern uint32_t input_counter;
+extern uint32_t output_counter;
+
+extern uint64_t NCO_Step_Size;
+
+extern uint64_t iteration;
+
+extern int64_t PPM_Offset;
+
+
+__host__ cudaError_t initialize_frf_coefficients_in_constant_memory(float *);
+
+__host__ uint32_t complex_test_signal_generator(uint32_t, uint32_t, float, complex64 **);
+
+__host__ uint32_t calculate_FRF_output_length(uint32_t, uint32_t,
+                                          uint64_t, uint64_t, uint64_t);
+
+__host__ uint64_t calculate_FRF_NCO_step_size(uint64_t, int64_t);
+
+__global__ void create_complex_input_vector(complex64* __restrict__, complex64* __restrict__,
+                                    uint32_t, uint16_t,
+                                    complex64* __restrict__);
+
+__global__ void complex_fractional_resampling_filter(const complex64* __restrict__,
+                                                       uint32_t,
+                                                       uint64_t,
+                                                       uint64_t,
+                                                       uint32_t,
+                                                       complex64* __restrict__);
+
+__host__ int post_run_NCO_update(uint32_t);
+__global__ void post_run_complex_state_update(uint32_t,
+                                              uint16_t,
+                                              complex64* __restrict__,
+                                              complex64* __restrict__);
+
+__host__ void initialize_state(complex64 *, uint16_t);
+
+// Convenience function for checking CUDA runtime API results
+// can be wrapped around any runtime API call. No-op in release builds.
+inline
+cudaError_t checkCuda(cudaError_t result)
+{
+#if defined(DEBUG) || defined(_DEBUG)
+  if (result != cudaSuccess) {
+    fprintf(stderr, "CUDA Runtime Error: %s\n",
+            cudaGetErrorString(result));
+    assert(result == cudaSuccess);
+  }
+#endif
+  return result;
+}
+
+
+#endif /* GPUFRACTIONALRESAMPLIGNFILTER_H_ */
